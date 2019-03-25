c1eb9c0d7f6709d660bc87404e1507e7e25ef8ac
Vincent Golle <vincent.golle@gmail.com>
2019-03-26 00:03:20 +0100

    Accelerometer can now be configured in both modes

--- source/sensors/lis2dw12_wrapper.c
+++ source/sensors/lis2dw12_wrapper.c
@@ -75,13 +75,13 @@ static void _lis2dw12_readout_cb(ret_code_t result, void * p_user_data) {
 		return;
 	}
 
-    acceleration_mg[0] = LIS2DW12_FROM_FS_8g_LP1_TO_mg(data_raw_acceleration->i16bit[0]);
-    acceleration_mg[1] = LIS2DW12_FROM_FS_8g_LP1_TO_mg(data_raw_acceleration->i16bit[1]);
-    acceleration_mg[2] = LIS2DW12_FROM_FS_8g_LP1_TO_mg(data_raw_acceleration->i16bit[2]);
+	acceleration_mg[0] = LIS2DW12_FROM_FS_8g_LP1_TO_mg(data_raw_acceleration->i16bit[0]);
+	acceleration_mg[1] = LIS2DW12_FROM_FS_8g_LP1_TO_mg(data_raw_acceleration->i16bit[1]);
+	acceleration_mg[2] = LIS2DW12_FROM_FS_8g_LP1_TO_mg(data_raw_acceleration->i16bit[2]);
 
 	m_is_updated = true;
 
-	LOG_DEBUG("BMG250 read");
+	LOG_DEBUG("LIS2DW read");
 
 }
 
@@ -110,7 +110,7 @@ static void _lis2dw12_read_sensor(void) {
 
 static void _int1_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
 {
-    // schedule sensor reading
+	// schedule sensor reading
 	_lis2dw12_read_sensor();
 }
 
@@ -127,7 +127,77 @@ void lis2dw12_wrapper_init(void)
 	 */
 	lis2dw12_device_id_get(&dev_ctx, &whoamI);
 	if (whoamI != LIS2DW12_ID)
-		LOG_ERROR("LIS12 not found");
+		LOG_ERROR("LIS2DW not found");
+
+	/*
+	 * Restore default configuration
+	 */
+	lis2dw12_reset_set(&dev_ctx, PROPERTY_ENABLE);
+	do {
+		lis2dw12_reset_get(&dev_ctx, &rst);
+	} while (rst);
+
+	/*
+	 *  Enable Block Data Update
+	 */
+	lis2dw12_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
+
+	/*
+	 * Set full scale
+	 */
+	lis2dw12_full_scale_set(&dev_ctx, LIS2DW12_8g);
+
+	/*
+	 * Configure filtering chain
+	 *
+	 * Accelerometer - filter path / bandwidth
+	 */
+	lis2dw12_filter_path_set(&dev_ctx, LIS2DW12_LPF_ON_OUT);
+	lis2dw12_filter_bandwidth_set(&dev_ctx, LIS2DW12_ODR_DIV_4);
+
+	/*
+	 * Configure power mode
+	 */
+	//lis2dw12_power_mode_set(&dev_ctx, LIS2DW12_HIGH_PERFORMANCE);
+	lis2dw12_power_mode_set(&dev_ctx, LIS2DW12_CONT_LOW_PWR_LOW_NOISE_12bit);
+
+	/*
+	 * Set Output Data Rate
+	 */
+	lis2dw12_data_rate_set(&dev_ctx, LIS2DW12_XL_ODR_25Hz);
+
+	/*
+	 * Enable interrupt generation on Wake-Up INT1 pin
+	 *
+	 */
+	lis2dw12_pin_int1_route_get(&dev_ctx, &int_route.ctrl4_int1_pad_ctrl);
+	int_route.ctrl4_int1_pad_ctrl.int1_wu = PROPERTY_ENABLE;
+	lis2dw12_pin_int1_route_set(&dev_ctx, &int_route.ctrl4_int1_pad_ctrl);
+
+	nrfx_gpiote_in_config_t in_config = NRFX_GPIOTE_CONFIG_IN_SENSE_LOTOHI(true);
+	in_config.pull = NRF_GPIO_PIN_PULLDOWN;
+
+	ret_code_t err_code = nrfx_gpiote_in_init(LIS_INT1, &in_config, _int1_handler);
+	APP_ERROR_CHECK(err_code);
+
+	nrfx_gpiote_in_event_enable(LIS_INT1, true);
+
+}
+
+void lis2dw12_wrapper_set_wake(void)
+{
+	uint8_t whoamI, rst;
+
+	dev_ctx.write_reg = _lis_i2c_write;
+	dev_ctx.read_reg = _lis_i2c_read;
+	dev_ctx.handle = NULL;
+
+	/*
+	 * Check device ID
+	 */
+	lis2dw12_device_id_get(&dev_ctx, &whoamI);
+	if (whoamI != LIS2DW12_ID)
+		LOG_ERROR("LIS2DW not found");
 
 	/*
 	 * Restore default configuration
@@ -150,7 +220,7 @@ void lis2dw12_wrapper_init(void)
 	/*
 	 * Set Output Data Rate
 	 */
-	lis2dw12_data_rate_set(&dev_ctx, LIS2DW12_XL_ODR_200Hz);
+	lis2dw12_data_rate_set(&dev_ctx, LIS2DW12_XL_ODR_1Hz6_LP_ONLY);
 
 	/*
 	 * Apply high-pass digital filter on Wake-Up function
@@ -173,20 +243,20 @@ void lis2dw12_wrapper_init(void)
 	lis2dw12_wkup_threshold_set(&dev_ctx, 2);
 
 	/*
-	 * Enable interrupt generation on Wake-Up INT1 pin
+	 * Enable interrupt generation on Wake-Up INT2 pin
 	 *
 	 */
-	lis2dw12_pin_int1_route_get(&dev_ctx, &int_route.ctrl4_int1_pad_ctrl);
-	int_route.ctrl4_int1_pad_ctrl.int1_wu = PROPERTY_ENABLE;
-	lis2dw12_pin_int1_route_set(&dev_ctx, &int_route.ctrl4_int1_pad_ctrl);
+	lis2dw12_pin_int2_route_get(&dev_ctx, &int_route.ctrl5_int2_pad_ctrl);
+	int_route.ctrl5_int2_pad_ctrl.int2_drdy = PROPERTY_ENABLE;
+	lis2dw12_pin_int2_route_set(&dev_ctx, &int_route.ctrl5_int2_pad_ctrl);
 
 	nrfx_gpiote_in_config_t in_config = NRFX_GPIOTE_CONFIG_IN_SENSE_LOTOHI(true);
 	in_config.pull = NRF_GPIO_PIN_PULLDOWN;
 
-	ret_code_t err_code = nrfx_gpiote_in_init(LIS_INT1, &in_config, _int1_handler);
+	ret_code_t err_code = nrfx_gpiote_in_init(LIS_INT2, &in_config, _int1_handler);
 	APP_ERROR_CHECK(err_code);
 
-	nrfx_gpiote_in_event_enable(LIS_INT1, true);
+	nrfx_gpiote_in_event_enable(LIS_INT2, true);
 
 }
 
